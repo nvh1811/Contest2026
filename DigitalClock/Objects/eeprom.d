dbc5ea77bc7b868749cb1f2123459e9499fa798c
DMTuan-dmt <Tuan.DM233695@sis.hust.edu.vn>
2026-04-22 22:39:02 +0700

    Delete DigitalClock directory

--- DigitalClock/Objects/eeprom.d
+++
@@ -1,5 +0,0 @@
-./objects/eeprom.o: Source\Module\EEPROM.c Source\Module\EEPROM.h \
-  C:\KeilC\SONiX\SN32F4_DFP\1.1.1\Device\Include\SN32F400.h \
-  C:\KeilC\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm0.h \
-  C:\KeilC\SONiX\SN32F4_DFP\1.1.1\Device\Include\system_SN32F400.h \
-  Source\Module\..\Driver\I2C.h
