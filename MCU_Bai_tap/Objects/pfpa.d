019e02459daa186c949b3a5fdaecb0a216124115
DMTuan-dmt <Tuan.DM233695@sis.hust.edu.vn>
2026-04-27 20:09:17 +0700

    Delete MCU_Bai_tap directory

--- MCU_Bai_tap/Objects/pfpa.d
+++
@@ -1,4 +0,0 @@
-./objects/pfpa.o: Source\Driver\PFPA.c \
-  C:\KeilC\SONiX\SN32F4_DFP\1.1.1\Device\Include\SN32F400.h \
-  C:\KeilC\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm0.h \
-  C:\KeilC\SONiX\SN32F4_DFP\1.1.1\Device\Include\system_SN32F400.h
